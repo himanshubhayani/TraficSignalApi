fefbd4ed0541e36caa039fc5f0ed8d230f266bef
himanshu bhayani <himanshu@bhayani.com>
2021-04-11 18:07:39 +0530

    signup page,create,update,delete user

--- Test.api/Controllers/UserController.cs
+++ Test.api/Controllers/UserController.cs
@@ -22,12 +22,56 @@ namespace Test.api.Controllers
     public class UserController : Controller
     {
         #region 'Constructor'
-        public UserController()
+        private readonly IUserService _userService;
+        MessageVM messageVM = new MessageVM();
+        public UserController(IUserService userService)
         {
-           
+            _userService = userService;
         }
         #endregion
 
-       
+        /// <summary>
+        /// This API called when user register.
+        /// </summary>
+        /// <param name="Model"> User Model</param>
+        /// <returns></returns>
+        [HttpPost("CreateUser")]
+        public async Task<OperationResult<Users>> CreateUser([FromBody] Users Model)
+        {
+            try
+            {
+                var result = await _userService.CheckInsertOrUpdateAsync(Model);
+                return new OperationResult<Users>(result);
+            }
+
+            catch (Exception ex)
+            {
+                messageVM.MessageType = Convert.ToInt32(MessageType.Error);
+                messageVM.Message = CommonMessage.DefaultErrorMessage;
+                return new OperationResult<Users>(messageVM, false);
+            }
+        }
+
+        /// <summary>
+        /// This API called for Get all active user.
+        /// </summary>
+        /// <param name="Model"> User Model</param>
+        /// <returns></returns>
+        [HttpGet("GetAllUser")]
+        public async Task<OperationResult<List<Users>>> GetAllActiveUser()
+        {
+            try
+            {
+                var result = await _userService.GetAllActiveUser();
+                return new OperationResult<List<Users>>(result);
+            }
+
+            catch (Exception ex)
+            {
+                messageVM.MessageType = Convert.ToInt32(MessageType.Error);
+                messageVM.Message = CommonMessage.DefaultErrorMessage;
+                return new OperationResult<List<Users>>(messageVM, false);
+            }
+        }
     }
 }
--- Test.data/Service/UserService.cs
+++ Test.data/Service/UserService.cs
@@ -8,6 +8,7 @@ namespace Test.data
     using System;
     using System.Threading.Tasks;
     using System.Data;
+    using System.Reflection;
 
     public partial class UserService : ServiceBase<Users>, IUserService
     {
@@ -17,11 +18,48 @@ namespace Test.data
             _uow = unitOfWork;
         }
 
+        public async Task<Users> CheckInsertOrUpdateAsync(Users user)
+        {
+            var existingItem = _uow.UserRepository.GetManyAsync(t => t.Id == user.Id).Result.FirstOrDefault();
 
+            if (existingItem != null)
+            {
+                return await UpdateUserAsync(existingItem, user);
+            }
+            else
+            {
+                return await InsertUserAsync(user);
+            }
+        }
+        public async Task<Users> UpdateUserAsync(Users existingItem, Users user)
+        {
+            foreach (PropertyInfo item in user.GetType().GetProperties())
+            {
+                if (!item.CanRead || (item.GetIndexParameters().Length > 0))
+                    continue;
+
+                PropertyInfo other = existingItem.GetType().GetProperty(item.Name);
+                if ((other != null) && (other.CanWrite))
+                    other.SetValue(existingItem, item.GetValue(user, null), null);
+            }
+
+            await _uow.UserRepository.UpdateAsync(existingItem, existingItem.Id);
+            return existingItem;
+        }
+        public async Task<Users> InsertUserAsync(Users user)
+        {
+            var newItem = await _uow.UserRepository.AddAsync(user);
+            return newItem;
+        }
+        public async Task<List<Users>> GetAllActiveUser()
+        {
+            return await _uow.UserRepository.GetManyAsync(x=>x.IsActive==true);
+        }
     }
 
     public partial interface IUserService : IService<Users>
     {
-       
+        Task<Users> CheckInsertOrUpdateAsync(Users user);
+        Task<List<Users>> GetAllActiveUser();
     }
 }
